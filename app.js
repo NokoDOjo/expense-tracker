7b145032e21922d38ae1c40ae87dd5d77875cf41
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-07-20 12:07:09 +0800

    feat: add home route and edit home page

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const methodOverride = require('method-override')
 require('./config/mongoose')
+
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
 
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
-app.set('view engine', '.hbs')
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
+app.use(routes)
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 app.listen(PORT, () => {
