1adcf47afdf58579705a9631fb3865011e7ced9f
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-07-23 18:34:20 +0800

    feat: add category filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,19 +6,31 @@ const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const categories = []
+  const filterCategory = req.query.filterCategory
+  const filter = {}
+  let categoryName = ""
+  if (filterCategory) { filter.category = filterCategory }
+
 
   Category.find()
     .lean()
     .then(category => categories.push(...category))
     .catch(error => console.log(error))
 
-  Record.find()
+  if (filterCategory.length !== 0 ) {
+  Category.findById(filterCategory)
+    .lean()
+    .then(category => categoryName = category.name)
+    .catch(error => console.log(error))
+  }
+  
+  Record.find(filter)
   .lean()
   .populate('category')
   .then((records) => {
     let totalAmount = 0
     records.forEach(record => totalAmount += record.amount)
-    res.render('index', { categories, records, totalAmount })
+    res.render('index', { categories, records, totalAmount, filterCategory, categoryName })
   })
   .catch(error => console.log(error))
 })
